44e43171ad26cf22d363b6d33c039aa91e6db446
Raphael Abreu <raphaelrat@hotmail.com>
2022-06-26 12:46:49 -0300

    add name and functions

--- fs.cpp
+++ fs.cpp
@@ -1,5 +1,63 @@
+ /**
+ * Aluno: Raphael Abreu Farias de Jesus
+ * Matrícula: 20150460
+ * Trabalho: Desenvolver um sistema de arquivos que simula padrão EXT3. O sistema deve ser desenvolvido em C ou C++ utilizando o compilador GNU GCC e chamadas de sistemas do padrão POSIX. 
+ * Disciplina: Arquitetura de Sistemas Operacionais - DEC7556
+ * Turma: 07655 
+ * Data de entrega: 03/07/2022
+ */
+
 /**
  * Implemente aqui as funções dos sistema de arquivos que simula EXT3
  */
 
 #include "fs.h"
+
+/**
+ * @brief Inicializa um sistema de arquivos que simula EXT3
+ * @param fsFileName nome do arquivo que contém sistema de arquivos que simula EXT3 (caminho do arquivo no sistema de arquivos local)
+ * @param blockSize tamanho em bytes do bloco
+ * @param numBlocks quantidade de blocos
+ * @param numInodes quantidade de inodes
+ */
+void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes){
+
+}
+
+/**
+ * @brief Adiciona um novo arquivo dentro do sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param filePath caminho completo novo arquivo dentro sistema de arquivos que simula EXT3.
+ * @param fileContent conteúdo do novo arquivo
+ */
+void addFile(std::string fsFileName, std::string filePath, std::string fileContent){
+
+}
+
+/**
+ * @brief Adiciona um novo diretório dentro do sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param dirPath caminho completo novo diretório dentro sistema de arquivos que simula EXT3.
+ */
+void addDir(std::string fsFileName, std::string dirPath){
+
+}
+
+/**
+ * @brief Remove um arquivo ou diretório (recursivamente) de um sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param path caminho completo do arquivo ou diretório a ser removido.
+ */
+void remove(std::string fsFileName, std::string path){
+
+}
+
+/**
+ * @brief Move um arquivo ou diretório em um sistema de arquivos que simula EXT3. O sistema já deve ter sido inicializado.
+ * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
+ * @param oldPath caminho completo do arquivo ou diretório a ser movido.
+ * @param newPath novo caminho completo do arquivo ou diretório.
+ */
+void move(std::string fsFileName, std::string oldPath, std::string newPath){
+    
+}
