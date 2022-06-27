a159b5acb133ca5f60d88b4b54b83999364197e9
Raphael Abreu <raphaelrat@hotmail.com>
2022-06-27 14:05:16 -0300

    Add initFs

--- fs.cpp
+++ fs.cpp
@@ -1,9 +1,9 @@
- /**
+/**
  * Aluno: Raphael Abreu Farias de Jesus
  * Matrícula: 20150460
- * Trabalho: Desenvolver um sistema de arquivos que simula padrão EXT3. O sistema deve ser desenvolvido em C ou C++ utilizando o compilador GNU GCC e chamadas de sistemas do padrão POSIX. 
+ * Trabalho: Desenvolver um sistema de arquivos que simula padrão EXT3. O sistema deve ser desenvolvido em C ou C++ utilizando o compilador GNU GCC e chamadas de sistemas do padrão POSIX.
  * Disciplina: Arquitetura de Sistemas Operacionais - DEC7556
- * Turma: 07655 
+ * Turma: 07655
  * Data de entrega: 03/07/2022
  */
 
@@ -13,15 +13,48 @@
 
 #include "fs.h"
 
-/**
- * @brief Inicializa um sistema de arquivos que simula EXT3
- * @param fsFileName nome do arquivo que contém sistema de arquivos que simula EXT3 (caminho do arquivo no sistema de arquivos local)
- * @param blockSize tamanho em bytes do bloco
- * @param numBlocks quantidade de blocos
- * @param numInodes quantidade de inodes
- */
-void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes){
+void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
+{
+    FILE *file = fopen(fsFileName.c_str(), "w+");
 
+    size_t sizeOfChar = sizeof(char);        // Size of a char
+    fwrite(&blockSize, sizeOfChar, 1, file); // Byte 1 - Size of the block
+    fwrite(&numBlocks, sizeOfChar, 1, file); // Byte 2 - Number of blocks
+    fwrite(&numInodes, sizeOfChar, 1, file); // Byte 3 - Number of Inodes
+
+    char firstByteOfMap = 0x01;                   // First block is used by root
+    fwrite(&firstByteOfMap, sizeOfChar, 1, file); // Byte 4 - Define block 0 as used because of root (first bit of Bitmap)
+
+    char sizeOfBitmap = (numBlocks - 1) / 8;                 // Size of the Bitmap (-1 because of root)
+    int sizeOfInodeVector = (numInodes - 1) * sizeof(INODE); // Size of the Inode Vector (-1 because of root)
+    char empty[sizeOfInodeVector];                           // Char vector filled with 0 to set 0 values on fwrite
+
+    for (int i = 0; i < sizeof(empty); ++i)
+    {
+        empty[i] = 0;
+    }
+
+    fwrite(&empty, sizeOfChar, sizeOfBitmap, file); // Byte 5 to Byte(5 + bitmap size -1) - Fill the rest of bitmap with 0 (all bits of bitmap)
+
+    INODE root = {
+        // First inode is the directory "/"
+        0x01,               // 0x01 if used, 0x00 if free
+        0x01,               // 0x01 if directory, 0x00 if file
+        "/",                // Name of file/directory
+        0x00,               // Size of file/directory in bytes
+        {0x00, 0x00, 0x00}, // Direct blocks
+        {0x00, 0x00, 0x00}, // Indirect blocks
+        {0x00, 0x00, 0x00}, // Double indirect blocks
+    };
+
+    fwrite(&root, sizeOfChar, sizeof(INODE), file);      // Add root inode in file
+    fwrite(&empty, sizeOfChar, sizeOfInodeVector, file); // Fill the rest of inodes with 0
+    fwrite(&empty, sizeOfChar, 1, file);                 // Index of root
+
+    int sizeOfBlockVector = numBlocks * blockSize;       // Size of the Block Vector
+    fwrite(&empty, sizeOfChar, sizeOfBlockVector, file); // Fill the block vector wuth 0 (because it's empty)
+
+    fclose(file);
 }
 
 /**
@@ -30,8 +63,8 @@ void initFs(std::string fsFileName, int blockSize, int numBlocks, int numInodes)
  * @param filePath caminho completo novo arquivo dentro sistema de arquivos que simula EXT3.
  * @param fileContent conteúdo do novo arquivo
  */
-void addFile(std::string fsFileName, std::string filePath, std::string fileContent){
-
+void addFile(std::string fsFileName, std::string filePath, std::string fileContent)
+{
 }
 
 /**
@@ -39,8 +72,8 @@ void addFile(std::string fsFileName, std::string filePath, std::string fileConte
  * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
  * @param dirPath caminho completo novo diretório dentro sistema de arquivos que simula EXT3.
  */
-void addDir(std::string fsFileName, std::string dirPath){
-
+void addDir(std::string fsFileName, std::string dirPath)
+{
 }
 
 /**
@@ -48,8 +81,8 @@ void addDir(std::string fsFileName, std::string dirPath){
  * @param fsFileName arquivo que contém um sistema sistema de arquivos que simula EXT3.
  * @param path caminho completo do arquivo ou diretório a ser removido.
  */
-void remove(std::string fsFileName, std::string path){
-
+void remove(std::string fsFileName, std::string path)
+{
 }
 
 /**
@@ -58,6 +91,6 @@ void remove(std::string fsFileName, std::string path){
  * @param oldPath caminho completo do arquivo ou diretório a ser movido.
  * @param newPath novo caminho completo do arquivo ou diretório.
  */
-void move(std::string fsFileName, std::string oldPath, std::string newPath){
-    
+void move(std::string fsFileName, std::string oldPath, std::string newPath)
+{
 }
